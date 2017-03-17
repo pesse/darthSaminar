2b3cc9a4c47ebfe559bb3be11274fe9a35a03225
Samuel Nitsche <snitsche@meyle-mueller.de>
2017-03-17 10:13:27 +0100

    Step 3

--- GWTJsInterop/JsInteropBox/src/main/java/de/apollon/darthSaminar/jsInterop/box/client/Box.java
+++ GWTJsInterop/JsInteropBox/src/main/java/de/apollon/darthSaminar/jsInterop/box/client/Box.java
@@ -10,9 +10,17 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 
 import de.apollon.darthSaminar.jsInterop.box.client.core.Document;
-import de.apollon.darthSaminar.jsInterop.box.client.threejs.PerspectiveCamera;
-import de.apollon.darthSaminar.jsInterop.box.client.threejs.Scene;
-import de.apollon.darthSaminar.jsInterop.box.client.threejs.WebGLRenderer;
+import de.pesse.gwt.jsinterop.threeJs.cameras.PerspectiveCamera;
+import de.pesse.gwt.jsinterop.threeJs.core.Geometry;
+import de.pesse.gwt.jsinterop.threeJs.geometries.BoxBufferGeometry;
+import de.pesse.gwt.jsinterop.threeJs.loaders.TextureLoader;
+import de.pesse.gwt.jsinterop.threeJs.materials.Material;
+import de.pesse.gwt.jsinterop.threeJs.materials.MeshBasicMaterial;
+import de.pesse.gwt.jsinterop.threeJs.materials.MeshBasicMaterialParameters;
+import de.pesse.gwt.jsinterop.threeJs.objects.Mesh;
+import de.pesse.gwt.jsinterop.threeJs.renderers.WebGLRenderer;
+import de.pesse.gwt.jsinterop.threeJs.scenes.Scene;
+import de.pesse.gwt.jsinterop.threeJs.textures.Texture;
 
 /**
  * @author snitsche
@@ -25,6 +33,7 @@ public class Box extends Composite implements AnimationCallback
 	private Canvas canvas;
 	private PerspectiveCamera camera;
 	private Scene scene;
+	private Mesh mesh;
 
 	/**
 	 *
@@ -40,25 +49,23 @@ public class Box extends Composite implements AnimationCallback
 	{
 
 		camera = new PerspectiveCamera(70, (double)Window.getClientWidth()/(double)Window.getClientHeight(), 1, 1000);
+		camera.position.z = 400;
+
 
 		scene = new Scene();
 
+		Texture texture = new TextureLoader().load("textures/crate.gif");
+		Geometry geometry = new BoxBufferGeometry(200, 200, 200);
+		Material material = new MeshBasicMaterial( new MeshBasicMaterialParameters.Builder()
+				.map(texture)
+				.build() );
+
+		mesh = new Mesh(geometry, material);
+
+		scene.add(mesh);
+
 		renderer = new WebGLRenderer();
 		renderer.setSize(Window.getClientWidth(), Window.getClientHeight());
-		//renderer.domElement = canvas.getCanvasElement();
-
-		// camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-		// camera.position.z = 400;
-		// scene = new THREE.Scene();
-		// var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-		// var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-		// var material = new THREE.MeshBasicMaterial( { map: texture } );
-		// mesh = new THREE.Mesh( geometry, material );
-		// scene.add( mesh );
-		// renderer = new THREE.WebGLRenderer();
-		// renderer.setPixelRatio( window.devicePixelRatio );
-		// renderer.setSize( window.innerWidth, window.innerHeight );
-		// document.body.appendChild( renderer.domElement );
 
 		Document.body.appendChild(renderer.domElement);
 
@@ -87,6 +94,8 @@ public class Box extends Composite implements AnimationCallback
 
 		AnimationScheduler.get().requestAnimationFrame(this);
 
+		mesh.rotation.x += 0.005;
+		mesh.rotation.y += 0.01;
 
 		renderer.render(scene, camera);
 	}
